b2fed4dc3aad1f8dd8846f2a719a662dc8aae12a
Ricardo Castro <mcc.ricardo@gmail.com>
2015-05-07 08:55:08 +0100

    Added migration to hold invites.

---
+++ alembic/versions/b27e19d4d63_create_invites_table.py
@@ -0,0 +1,30 @@
+"""Create invites table
+
+Revision ID: b27e19d4d63
+Revises: 4bd0784e2978
+Create Date: 2015-05-07 08:40:08.023642
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'b27e19d4d63'
+down_revision = '4bd0784e2978'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'invites',
+        sa.Column('users_id', sa.Integer, sa.ForeignKey('users.id'),
+                  nullable=False),
+        sa.Column('pulse_users_id', sa.Integer,
+                  sa.ForeignKey('pulse_users.id'), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_table('invites')
